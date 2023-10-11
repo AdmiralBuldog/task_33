7e0343f1eac15eff463264b5e60f5d3147cab32c
AdmiralBuldog <133498688+AdmiralBuldog@users.noreply.github.com>
2023-10-12 01:15:29 +0300

    Update test_case_020_checking_feedback_page.py

--- test_case_020_checking_feedback_page.py
+++ test_case_020_checking_feedback_page.py
@@ -5,17 +5,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-@pytest.fixture
+@pytest.fixture  # Определение фикстуры для инициализации и завершения работы драйвера
 def driver():
-    driver = webdriver.Chrome()
-    yield driver
-    driver.quit()
+    driver = webdriver.Chrome()  # Создание экземпляра драйвера Chrome
+    yield driver  # Возврат драйвера тестовой функции и ожидание завершения теста
+    driver.quit()  # Закрытие браузера после завершения теста
 
+def test_feedback_page(driver):  # Определение тестовой функции для проверки страницы обратной связи
+    driver.get("https://rt-internet.ru/help#/quiz-choice")  # Открытие страницы обратной связи
 
-def test_feedback_page(driver):
-    driver.get("https://rt-internet.ru/help#/quiz-choice")
-
-    # Проверка наличия различных форм обратной связи и их отображения
+    # Словарь селекторов для проверки наличия и отображения различных форм обратной связи
     feedback_boxes = {
         "Социальные сети": "#i11317",
         "Телефон поддержки": "#i11339",
@@ -25,44 +24,47 @@ def test_feedback_page(driver):
         "Сайт поддержки": "#i11483"
     }
 
+    # Проверка наличия и отображения различных форм обратной связи
     for box_name, selector in feedback_boxes.items():
         element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, selector)))
         assert element.is_displayed(), f"{box_name} box is not displayed"
 
-        # Проверка кликабельности иконок социальных сетей
+    # Проверка кликабельности иконок социальных сетей
     ok_icon = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, "#b11324 > img"))
     )
-    assert ok_icon.is_displayed()
+    assert ok_icon.is_displayed()  # Проверка отображения иконки Одноклассников
 
     vk_icon = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, "#b11322 > img"))
     )
-    assert vk_icon.is_displayed()
+    assert vk_icon.is_displayed()  # Проверка отображения иконки ВКонтакте
 
+    # Проверка отображения и активности ссылки на номер телефона
     phone_number = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, "#b11342 > p > a"))
     )
     assert phone_number.text == "8 800 100 08 00"
     assert phone_number.is_enabled()
 
+    # Проверка активности ссылки на чат Viber
     viber_link = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, "#b11329 > p > a:nth-child(2)"))
     )
     assert viber_link.is_enabled()
 
-    # Проверка ссылок в боксах
+    # Проверка корректности ссылок в боксах
     email_link = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, "#i11335 a"))
     )
-    assert email_link.get_attribute("href") == "https://lk.rt.ru/"
+    assert email_link.get_attribute("href") == "https://lk.rt.ru/"  # Проверка ссылки на E-mail
 
     offices_link = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, "#i11343 a"))
     )
-    assert offices_link.get_attribute("href") == "https://rt.ru/sale-office"
+    assert offices_link.get_attribute("href") == "https://rt.ru/sale-office"  # Проверка ссылки на Офисы
 
     support_site_link = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, "#i11483 a"))
     )
-    assert support_site_link.get_attribute("href") == "https://moscow.old.rt.ru/service"
+    assert support_site_link.get_attribute("href") == "https://moscow.old.rt.ru/service"  # Проверка ссылки на Сайт поддержки
