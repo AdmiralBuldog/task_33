00d871631b15ad3d235bd1231f0ca5fb46beb0be
AdmiralBuldog <133498688+AdmiralBuldog@users.noreply.github.com>
2023-10-12 01:19:48 +0300

    Update test_case_010_load_time.py

--- test_case_010_load_time.py
+++ test_case_010_load_time.py
@@ -3,11 +3,12 @@ import requests
 import time
 
 
-@pytest.mark.parametrize('url', ['https://rt-internet.ru/'])
-def test_page_load_time(url):
-    start_time = time.time()
-    response = requests.get(url)
-    elapsed_time = time.time() - start_time
+@pytest.mark.parametrize('url', ['https://rt-internet.ru/'])  # Использование декоратора parametrize для параметризации теста
+def test_page_load_time(url):  # Определение функции теста для измерения времени загрузки страницы
+    start_time = time.time()  # Засечка времени начала загрузки страницы
+    response = requests.get(url)  # Отправка HTTP-запроса GET на указанный URL
+    elapsed_time = time.time() - start_time  # Вычисление затраченного времени на загрузку страницы
+
 
     # Проверка статуса ответа (должен быть 200 OK)
     assert response.status_code == 200, f"Ошибка: получен статус {response.status_code}"
